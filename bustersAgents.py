878497db1a925009fff71dc535d5e21517f27c23
JiajunRen33 <77767850+JiajunRen33@users.noreply.github.com>
2021-04-21 20:57:15 -0400

    Add files via upload

---
+++ bustersAgents.py
@@ -0,0 +1,207 @@
+# bustersAgents.py
+# ----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+import util
+from game import Agent
+from game import Directions
+from keyboardAgents import KeyboardAgent
+import inference
+import busters
+
+class NullGraphics:
+    "Placeholder for graphics"
+    def initialize(self, state, isBlue = False):
+        pass
+    def update(self, state):
+        pass
+    def pause(self):
+        pass
+    def draw(self, state):
+        pass
+    def updateDistributions(self, dist):
+        pass
+    def finish(self):
+        pass
+
+class KeyboardInference(inference.InferenceModule):
+    """
+    Basic inference module for use with the keyboard.
+    """
+    def initializeUniformly(self, gameState):
+        "Begin with a uniform distribution over ghost positions."
+        self.beliefs = util.Counter()
+        for p in self.legalPositions: self.beliefs[p] = 1.0
+        self.beliefs.normalize()
+
+    def observeUpdate(self, observation, gameState):
+        noisyDistance = observation
+        pacmanPosition = gameState.getPacmanPosition()
+        allPossible = util.Counter()
+        for p in self.legalPositions:
+            trueDistance = util.manhattanDistance(p, pacmanPosition)
+            if noisyDistance != None and \
+                    busters.getObservationProbability(noisyDistance, trueDistance) > 0:
+                allPossible[p] = 1.0
+        allPossible.normalize()
+        self.beliefs = allPossible
+
+    def elapseTime(self, gameState):
+        pass
+
+    def getBeliefDistribution(self):
+        return self.beliefs
+
+
+class BustersAgent:
+    "An agent that tracks and displays its beliefs about ghost positions."
+
+    def __init__( self, index = 0, inference = "ExactInference", ghostAgents = None, observeEnable = True, elapseTimeEnable = True):
+        try:
+            inferenceType = util.lookup(inference, globals())
+        except Exception:
+            inferenceType = util.lookup('inference.' + inference, globals())
+        self.inferenceModules = [inferenceType(a) for a in ghostAgents]
+        self.observeEnable = observeEnable
+        self.elapseTimeEnable = elapseTimeEnable
+
+    def registerInitialState(self, gameState):
+        "Initializes beliefs and inference modules"
+        import __main__
+        self.display = __main__._display
+        for inference in self.inferenceModules:
+            inference.initialize(gameState)
+        self.ghostBeliefs = [inf.getBeliefDistribution() for inf in self.inferenceModules]
+        self.firstMove = True
+
+    def observationFunction(self, gameState):
+        "Removes the ghost states from the gameState"
+        agents = gameState.data.agentStates
+        gameState.data.agentStates = [agents[0]] + [None for i in range(1, len(agents))]
+        return gameState
+
+    def getAction(self, gameState):
+        "Updates beliefs, then chooses an action based on updated beliefs."
+        for index, inf in enumerate(self.inferenceModules):
+            if not self.firstMove and self.elapseTimeEnable:
+                inf.elapseTime(gameState)
+            self.firstMove = False
+            if self.observeEnable:
+                inf.observe(gameState)
+            self.ghostBeliefs[index] = inf.getBeliefDistribution()
+        self.display.updateDistributions(self.ghostBeliefs)
+        return self.chooseAction(gameState)
+
+    def chooseAction(self, gameState):
+        "By default, a BustersAgent just stops.  This should be overridden."
+        return Directions.STOP
+
+class BustersKeyboardAgent(BustersAgent, KeyboardAgent):
+    "An agent controlled by the keyboard that displays beliefs about ghost positions."
+
+    def __init__(self, index = 0, inference = "KeyboardInference", ghostAgents = None):
+        KeyboardAgent.__init__(self, index)
+        BustersAgent.__init__(self, index, inference, ghostAgents)
+
+    def getAction(self, gameState):
+        return BustersAgent.getAction(self, gameState)
+
+    def chooseAction(self, gameState):
+        return KeyboardAgent.getAction(self, gameState)
+
+from distanceCalculator import Distancer
+from game import Actions
+from game import Directions
+
+class GreedyBustersAgent(BustersAgent):
+    "An agent that charges the closest ghost."
+
+    def registerInitialState(self, gameState):
+        "Pre-computes the distance between every two points."
+        BustersAgent.registerInitialState(self, gameState)
+        self.distancer = Distancer(gameState.data.layout, False)
+
+    def chooseAction(self, gameState):
+        """
+        First computes the most likely position of each ghost that has
+        not yet been captured, then chooses an action that brings
+        Pacman closest to the closest ghost (according to mazeDistance!).
+        """
+        pacmanPosition = gameState.getPacmanPosition()
+        legal = [a for a in gameState.getLegalPacmanActions()]
+        livingGhosts = gameState.getLivingGhosts()
+        livingGhostPositionDistributions = \
+            [beliefs for i, beliefs in enumerate(self.ghostBeliefs)
+             if livingGhosts[i+1]]
+        """
+        Calculate the most likely position of each ghost - the lattice with the highest response index. 
+        """
+        min_distance_pos = None
+        for positionDistribution in livingGhostPositionDistributions:
+            max_key = None
+            #max_key is the most likely location
+            for key in positionDistribution.keys():
+                if max_key == None or positionDistribution[key] > positionDistribution[max_key]:
+                    max_key = key
+            #calculate the distance between max_key and the current location, find the shortest distance
+            if max_key != None:
+                distance = self.distancer.getDistance(pacmanPosition, max_key)
+                #inally min_distance_pos save the most likely ghost location and the shortest distance.
+                if min_distance_pos == None or min_distance_pos[1] > distance:
+                    min_distance_pos = [max_key, distance]
+        max_position = min_distance_pos[0]
+        #Calculate the transverse and longitudinal distances
+        x = max_position[0] - pacmanPosition[0]
+        y = max_position[1] - pacmanPosition[1]
+        suitable = []
+        #If the transverse distance is higher than the longitudinal distance, go east-west.
+        if x > 0:
+            if y > 0:
+                if y > x:
+                    suitable.append('North')
+                    suitable.append('East')
+                else:
+                    suitable.append('East')
+                    suitable.append('North')
+            else:
+                if -y > x:
+                    suitable.append('South')
+                    suitable.append('East')
+                else:
+                    suitable.append('East')
+                    suitable.append('South')
+        #If the longitudinal distance is higher than the transverse distance, go north-south.
+        else:
+            if y > 0:
+                if y > -x:
+                    suitable.append('North')
+                    suitable.append('West')
+                else:
+                    suitable.append('West')
+                    suitable.append('North')
+            else:
+                if -y > -x:
+                    suitable.append('South')
+                    suitable.append('West')
+                else:
+                    suitable.append('West')
+                    suitable.append('South')
+        #If there is an optimal direction in legal, go there
+        if suitable[0] in legal:
+            return suitable[0]
+        #If not, go in the second best direction
+        if suitable[1] in legal:
+            return suitable[1]
+        #If don't have a second best direction, go in any direction
+        return legal[0]
+    
---
+++ inference.py
@@ -0,0 +1,358 @@
+# inference.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+import itertools
+import random
+import busters
+import game
+
+from util import manhattanDistance, raiseNotDefined
+
+
+class DiscreteDistribution(dict):
+    """
+    A DiscreteDistribution models belief distributions and weight distributions
+    over a finite set of discrete keys.
+    """
+    def __getitem__(self, key):
+        self.setdefault(key, 0)
+        return dict.__getitem__(self, key)
+
+    def copy(self):
+        """
+        Return a copy of the distribution.
+        """
+        return DiscreteDistribution(dict.copy(self))
+
+    def argMax(self):
+        """
+        Return the key with the highest value.
+        """
+        if len(self.keys()) == 0:
+            return None
+        all = list(self.items())
+        values = [x[1] for x in all]
+        maxIndex = values.index(max(values))
+        return all[maxIndex][0]
+
+    def total(self):
+        """
+        Return the sum of values for all keys.
+        """
+        return float(sum(self.values()))
+
+    def normalize(self):
+        """
+        Normalize the distribution such that the total value of all keys sums
+        to 1. The ratio of values for all keys will remain the same. In the case
+        where the total value of the distribution is 0, do nothing.
+
+        >>> dist = DiscreteDistribution()
+        >>> dist['a'] = 1
+        >>> dist['b'] = 2
+        >>> dist['c'] = 2
+        >>> dist['d'] = 0
+        >>> dist.normalize()
+        >>> list(sorted(dist.items()))
+        [('a', 0.2), ('b', 0.4), ('c', 0.4), ('d', 0.0)]
+        >>> dist['e'] = 4
+        >>> list(sorted(dist.items()))
+        [('a', 0.2), ('b', 0.4), ('c', 0.4), ('d', 0.0), ('e', 4)]
+        >>> empty = DiscreteDistribution()
+        >>> empty.normalize()
+        >>> empty
+        {}
+        """
+        "*** YOUR CODE HERE ***"
+        # this script iterates the keys and values on the given dictionary and 
+        #  updatethe origion distribution p by p / sum_of_probabilites
+        dist_sum = self.total()
+        # if the distributions are all 0s or the distribution is empty, do nothing
+        #  else, we excute the normalize process.
+        if dist_sum != 0 and dist_sum!=None:
+            for key, val in self.items():
+                self[key] = val / dist_sum
+
+    def sample(self):
+        """
+        Draw a random sample from the distribution and return the key, weighted
+        by the values associated with each key.
+
+        >>> dist = DiscreteDistribution()
+        >>> dist['a'] = 1
+        >>> dist['b'] = 2
+        >>> dist['c'] = 2
+        >>> dist['d'] = 0
+        >>> N = 100000.0
+        >>> samples = [dist.sample() for _ in range(int(N))]
+        >>> round(samples.count('a') * 1.0/N, 1)  # proportion of 'a'
+        0.2
+        >>> round(samples.count('b') * 1.0/N, 1)
+        0.4
+        >>> round(samples.count('c') * 1.0/N, 1)
+        0.4
+        >>> round(samples.count('d') * 1.0/N, 1)
+        0.0
+        """
+        "*** YOUR CODE HERE ***"
+        # This script will draw a sample with probability relative to it's weight.
+        # It first generate a random number by uniform distribution in the range of 
+        #   [0,1), then multiply the decimal with the sum of the values in the given 
+        #   dictionary (rand_res). With "rand_res", it iterates over the  dictionary to 
+        #   compare with the cumunalted distribution value ("cum_val"). 
+        # If the "cum_val" is smaller than the "rand_res", the "cum_val" is added by 
+        #   the next value in the dictionary. If the "cum_val" is greater than the 
+        #   "rand_res", then the corresponding key (the kth in the loop) in the dictionary 
+        #   is returned.
+        # The necessary variables are defined outside the loop to imporve the performance.
+        dic_vals =  list(self.values())
+        rand_res = random.random() * sum(dic_vals)
+        cum_val = dic_vals[0]
+        for k in range(len(dic_vals)):
+            if rand_res < cum_val:
+                return list(self.keys())[k]
+            cum_val+=dic_vals[k+1]
+
+
+class InferenceModule:
+    """
+    An inference module tracks a belief distribution over a ghost's location.
+    """
+    ############################################
+    # Useful methods for all inference modules #
+    ############################################
+
+    def __init__(self, ghostAgent):
+        """
+        Set the ghost agent for later access.
+        """
+        self.ghostAgent = ghostAgent
+        self.index = ghostAgent.index
+        self.obs = []  # most recent observation position
+
+    def getJailPosition(self):
+        return (2 * self.ghostAgent.index - 1, 1)
+
+    def getPositionDistributionHelper(self, gameState, pos, index, agent):
+        try:
+            jail = self.getJailPosition()
+            gameState = self.setGhostPosition(gameState, pos, index + 1)
+        except TypeError:
+            jail = self.getJailPosition(index)
+            gameState = self.setGhostPositions(gameState, pos)
+        pacmanPosition = gameState.getPacmanPosition()
+        ghostPosition = gameState.getGhostPosition(index + 1)  # The position you set
+        dist = DiscreteDistribution()
+        if pacmanPosition == ghostPosition:  # The ghost has been caught!
+            dist[jail] = 1.0
+            return dist
+        pacmanSuccessorStates = game.Actions.getLegalNeighbors(pacmanPosition, \
+                gameState.getWalls())  # Positions Pacman can move to
+        if ghostPosition in pacmanSuccessorStates:  # Ghost could get caught
+            mult = 1.0 / float(len(pacmanSuccessorStates))
+            dist[jail] = mult
+        else:
+            mult = 0.0
+        actionDist = agent.getDistribution(gameState)
+        for action, prob in actionDist.items():
+            successorPosition = game.Actions.getSuccessor(ghostPosition, action)
+            if successorPosition in pacmanSuccessorStates:  # Ghost could get caught
+                denom = float(len(actionDist))
+                dist[jail] += prob * (1.0 / denom) * (1.0 - mult)
+                dist[successorPosition] = prob * ((denom - 1.0) / denom) * (1.0 - mult)
+            else:
+                dist[successorPosition] = prob * (1.0 - mult)
+        return dist
+
+    def getPositionDistribution(self, gameState, pos, index=None, agent=None):
+        """
+        Return a distribution over successor positions of the ghost from the
+        given gameState. You must first place the ghost in the gameState, using
+        setGhostPosition below.
+        """
+        if index == None:
+            index = self.index - 1
+        if agent == None:
+            agent = self.ghostAgent
+        return self.getPositionDistributionHelper(gameState, pos, index, agent)
+
+    def getObservationProb(self, noisyDistance, pacmanPosition, ghostPosition, jailPosition):
+        """
+        Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
+        """
+        "*** YOUR CODE HERE ***"
+        # This script calculates the manhattan Distance by calling function "manhattanDistance".
+        # Then the manhattan distance and the noisyDistance are passed as paratemers to the
+        #  "getObservationProbability" function to calculate the conditional probability
+        #  of P(noisyDistance|trueDistance). And this condistional probability will be 
+        #  returned as a result.
+        # The given special cases are detected by determing if ghost's position equals the 
+        # jail position and the observation is none. If yes, it will return a probability of 1.
+        # Else if ONLY noisyDistance is None or  ONLY ghost's position is at jail position, returns 0.
+    
+        if noisyDistance == None and ghostPosition == jailPosition: return 1
+        if noisyDistance == None or ghostPosition == jailPosition: return 0
+        # find the manhattan distance
+        mht_dis = manhattanDistance(pacmanPosition, ghostPosition)
+        # find the conditional probabilities
+        return busters.getObservationProbability(noisyDistance, trueDistance = mht_dis)
+    
+    def setGhostPosition(self, gameState, ghostPosition, index):
+        """
+        Set the position of the ghost for this inference module to the specified
+        position in the supplied gameState.
+
+        Note that calling setGhostPosition does not change the position of the
+        ghost in the GameState object used for tracking the true progression of
+        the game.  The code in inference.py only ever receives a deep copy of
+        the GameState object which is responsible for maintaining game state,
+        not a reference to the original object.  Note also that the ghost
+        distance observations are stored at the time the GameState object is
+        created, so changing the position of the ghost will not affect the
+        functioning of observe.
+        """
+        conf = game.Configuration(ghostPosition, game.Directions.STOP)
+        gameState.data.agentStates[index] = game.AgentState(conf, False)
+        return gameState
+
+    def setGhostPositions(self, gameState, ghostPositions):
+        """
+        Sets the position of all ghosts to the values in ghostPositions.
+        """
+        for index, pos in enumerate(ghostPositions):
+            conf = game.Configuration(pos, game.Directions.STOP)
+            gameState.data.agentStates[index + 1] = game.AgentState(conf, False)
+        return gameState
+
+    def observe(self, gameState):
+        """
+        Collect the relevant noisy distance observation and pass it along.
+        """
+        distances = gameState.getNoisyGhostDistances()
+        if len(distances) >= self.index:  # Check for missing observations
+            obs = distances[self.index - 1]
+            self.obs = obs
+            self.observeUpdate(obs, gameState)
+
+    def initialize(self, gameState):
+        """
+        Initialize beliefs to a uniform distribution over all legal positions.
+        """
+        self.legalPositions = [p for p in gameState.getWalls().asList(False) if p[1] > 1]
+        self.allPositions = self.legalPositions + [self.getJailPosition()]
+        self.initializeUniformly(gameState)
+
+    ######################################
+    # Methods that need to be overridden #
+    ######################################
+
+    def initializeUniformly(self, gameState):
+        """
+        Set the belief state to a uniform prior belief over all positions.
+        """
+        raise NotImplementedError
+
+    def observeUpdate(self, observation, gameState):
+        """
+        Update beliefs based on the given distance observation and gameState.
+        """
+        raise NotImplementedError
+
+    def elapseTime(self, gameState):
+        """
+        Predict beliefs for the next time step from a gameState.
+        """
+        raise NotImplementedError
+
+    def getBeliefDistribution(self):
+        """
+        Return the agent's current belief state, a distribution over ghost
+        locations conditioned on all evidence so far.
+        """
+        raise NotImplementedError
+
+
+class ExactInference(InferenceModule):
+    """
+    The exact dynamic inference module should use forward algorithm updates to
+    compute the exact belief function at each time step.
+    """
+    def initializeUniformly(self, gameState):
+        """
+        Begin with a uniform distribution over legal ghost positions (i.e., not
+        including the jail position).
+        """
+        self.beliefs = DiscreteDistribution()
+        for p in self.legalPositions:
+            self.beliefs[p] = 1.0
+        self.beliefs.normalize()
+
+    def observeUpdate(self, observation, gameState):
+        """
+        Update beliefs based on the distance observation and Pacman's position.
+
+        The observation is the noisy Manhattan distance to the ghost you are
+        tracking.
+
+        self.allPositions is a list of the possible ghost positions, including
+        the jail position. You should only consider positions that are in
+        self.allPositions.
+
+        The update model is not entirely stationary: it may depend on Pacman's
+        current position. However, this is not a problem, as Pacman's current
+        position is known.
+        """
+        "*** YOUR CODE HERE ***"
+        # The script first find the conditional probability of NoisyReading using
+        #  the function getObservationProb.
+        # Then find the posterior distribution using Bayes' rule
+        #   P(Real position|NoisyReading) = P(noisyDistance|Real position)P(Real position)
+        # After updating the dictionary by the for loop, the new dictionary is then 
+        #   normalized to ensure the sum of the distribution is 1
+        self.beliefs.normalize()
+        pcm_pos = gameState.getPacmanPosition()
+        jail_pos = self.getJailPosition()
+        for key in self.allPositions:
+            # P(Real position|NoisyReading) = P(noisyDistance|Real position)*P(Real position)
+            self.beliefs[key] = self.getObservationProb(observation, pcm_pos, key, jail_pos)*self.beliefs[key]
+        self.beliefs.normalize()
+        
+    def elapseTime(self, gameState):
+        """
+        Predict beliefs in response to a time step passing from the current
+        state.
+
+        The transition model is not entirely stationary: it may depend on
+        Pacman's current position. However, this is not a problem, as Pacman's
+        current position is known.
+        """
+        "*** YOUR CODE HERE ***"
+        
+        #set the DiscreteDistribution object
+        Dist = DiscreteDistribution()
+        for p in self.allPositions:
+            #get new positions for the ghost
+            newPosDist = self.getPositionDistribution(gameState, p)
+            #original probability
+            original_Prob = self.beliefs[p]
+            #get new beliefs
+            for new_p in newPosDist.keys():
+                #newProbability += original probability at time t * new probability at time t+1
+                Dist[new_p] += original_Prob * newPosDist[new_p]
+                #update belief at every position
+        self.beliefs = Dist
+
+
+    def getBeliefDistribution(self):
+        return self.beliefs
